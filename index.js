8eb8869a057c2f3a4930682a10e9c4452429890f
Mario Alves <marioalvesneto@hotmail.com>
2019-09-11 14:00:59 -0300

    Desafio Bônus de NodeJS

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var app = express();
+var data = new Date();
+
+var requestTime = function (req, res, next) {
+  var hour = data.getHours();
+  var min = data.getMinutes();
+  req.requestTime = ""+hour+":"+min;
+  next();
+}
+
+app.use(requestTime);
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  var responseText = 'Hello World';
+  responseText += ' ' + req.requestTime + '';
+  res.send(responseText);
 });
 
 var port = process.env.PORT || 3000;
